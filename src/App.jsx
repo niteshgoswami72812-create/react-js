9eb41699722ceac025f677563b5e494bd21202f4
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-04-24 15:36:55 +0530

    niteh

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Navbar from "./navbar.jsx";
-import { Footer } from "./footer.jsx";
+import Navbar from "./Navbar.jsx";
+import { Footer } from "./Footer.jsx";
+import image from "./assets/vite.svg"
 
 let App=()=>{
+
+  // javascript
+  let myname="Nitesh"
+
+
   return(
     <div>
       <Navbar />
       <h1>This Is Home Page</h1>
+      <img src={image} alt="" />
+      <img src="favicon.svg" alt="" />
+      <h2>my name is {myname}</h2>
       <Footer />
     </div>
   )
