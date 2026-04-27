6d05ddbbd55c9839ea69fb1e6d8f0f89524354c1
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-04-27 13:48:35 +0530

    nitesh

--- Level2/src/App.jsx
+++ Level2/src/App.jsx
@@ -4,10 +4,10 @@ let App = ()=>{
 
 
   return(
-    <div>
+    <div> 
 
       <Navbar/>
-      <h1>This is lenovo</h1>
+      <h1>This is lenovo IDEA PAD SLIM 3</h1>
 
       <p>My Laptop Name is</p>
     </div>
