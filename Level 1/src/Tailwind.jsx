4bb2ca54c625a4f309b6fddf1dcb2a49146fa058
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-05-01 13:02:32 +0530

    nn

--- Level 1/src/Tailwind.jsx
+++ Level 1/src/Tailwind.jsx
@@ -7,7 +7,7 @@ const Tailwind = () => {
    
 
    <div className='w-full border  '>
-    <ul className='flex justify-center items-center gap-10 h-20 bg-purple-400'>
+    <ul className='flex justify-center items-center gap-10 h-20 bg-purple-400 '>
             <li> <a href="#">Home</a></li>
             <li> <a href="contact.html">Contact</a></li>
             <li> <a href="">Pricing</a></li>
